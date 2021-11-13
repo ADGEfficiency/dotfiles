e46f3298384e45619c0469f70f1a52e5a910b787
Adam Green <adam.green@adgefficiency.com>
2021-11-14 02:28:46 +1300

    make install require less yes

--- ubuntu/main.sh
+++ ubuntu/main.sh
@@ -1,11 +1,11 @@
-sudo apt update
-sudo apt upgrade
+sudo apt -y update
+sudo apt -y upgrade
 sudo apt -y install build-essential
-sudo apt install git tree gnome-tweaks curl tree tmux fzf
+sudo apt -y install git tree gnome-tweaks curl tree tmux
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-./ubuntu/vim
-./ubuntu/rust
-./ubuntu/pyenv
+./vim
+./rust
+./pyenv
