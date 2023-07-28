7472ab6c91f7ad42975e1e7eb0d4a06999d4c728
Adam Green <adam.green@adgefficiency.com>
2023-07-28 21:19:43 +1200

    ipython config

---
+++ .ipython/start.py
@@ -0,0 +1,13 @@
+import os
+
+try:
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+except:
+    pass
+
+os.system('clear')
