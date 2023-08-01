18e92e643fa5a4da126a3d89e706ac67b9dca3bb
Adam Green <adam.green@adgefficiency.com>
2023-08-02 07:32:29 +1200

    turn on some stuff

--- .ipython/start.py
+++ .ipython/start.py
@@ -1,13 +1,15 @@
 import os
 
 try:
+    import matplotlib.pyplot as plt
     import numpy as np
     import pandas as pd
-    import matplotlib.pyplot as plt
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
-except:
+
+    pd.set_option("display.max_rows", 500)
+    pd.set_option("display.max_columns", 500)
+    pd.set_option("display.width", 1000)
+
+except Exception:
     pass
 
-os.system('clear')
+os.system("clear")
