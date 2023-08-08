ecfb26a5b07a817adec7936e9367c4520f4b0a10
Adam Green <adam.green@adgefficiency.com>
2023-08-08 15:33:27 +1200

    ci(nix-install): Debugging initial setup of Nix - include default.nix

---
+++ nix/default.nix
@@ -0,0 +1,39 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+in
+with pkgs; [
+  # Shell stuff
+  atuin
+  bat
+  direnv
+  exa
+  fzf
+  gh
+  gnugrep
+  gnumake
+  gnused
+  jq
+  lazydocker
+  lazygit
+  ripgrep
+  shunit2
+  starship
+  tmux
+  toybox
+  tree
+  zoxide
+  zsh
+  zsh-prezto
+  # Programming things
+  flyctl
+  git
+  hack-font
+  neovim
+  nodejs_18
+  python310
+  ruby
+  vimPlugins.packer-nvim
+]
+++ lib.optionals isDarwin [ locale ] # Add macOS-specific packages conditionally
