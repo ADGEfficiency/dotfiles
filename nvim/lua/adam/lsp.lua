6abc6f820ca4df320e4e9de9538095f6b64e27ab
Adam Green <adam.green@adgefficiency.com>
2023-09-20 14:26:02 +1200

    refactor: move mason into lsp

--- nvim/lua/adam/lsp.lua
+++ nvim/lua/adam/lsp.lua
@@ -1,3 +1,39 @@
+-- mason setup
+local lspconfig = require("lspconfig")
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"pyright",
+		"tailwindcss",
+		"ruff_lsp",
+		"bashls",
+		"prosemd_lsp",
+		"marksman",
+		"lua_ls",
+		"html",
+		"docker_compose_language_service",
+		"ruff_lsp",
+		"bashls",
+		"dockerls",
+		"jsonls",
+		"prosemd_lsp",
+		"rnix",
+		"tsserver",
+		"emmet_language_server",
+		"pyright",
+		"tailwindcss",
+		"rust_analyzer",
+		"jedi_language_server",
+	},
+	automatic_installation = true,
+})
+
+require("mason-lspconfig").setup_handlers({
+	function(server)
+		lspconfig[server].setup({})
+	end,
+})
 -- Key Mappings
 
 local opts = { noremap = true, silent = true }
