07302818c4740a6cf30207658181b21f998bc1ce
Adam Green <adam.green@adgefficiency.com>
2023-10-01 16:42:17 +1300

    ci: fix nix-setup bug in ci

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -3,21 +3,22 @@
 DOTFILES=$HOME/dotfiles/dotfiles
 
 setup_dotfile() {
-    local fi=$1
-    echo "Setting up ~/$fi"
-    echo "source $DOTFILES/$fi" >> "$HOME/$fi"
-    cat "$HOME/$fi"
+  local fi=$1
+  echo "Setting up ~/$fi"
+  echo "source $DOTFILES/$fi" >> "$HOME/$fi"
+  cat "$HOME/$fi"
 }
 
-setup_dotfile .bashrc
-setup_dotfile .zshrc
-setup_dotfile .zshenv
-setup_dotfile .zprofile
-setup_dotfile .vimrc
-setup_dotfile .tmux.conf
+# setup_dotfile .bashrc
+# setup_dotfile .zshrc
+# setup_dotfile .zshenv
+# setup_dotfile .zprofile
+# setup_dotfile .vimrc
+# setup_dotfile .tmux.conf
 
-echo "setting up git"
-cp "$HOME/dotfiles/git/.gitignore" "$HOME/.gitignore"
-cp "$HOME/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+echo "source $DOTFILES/yabai/yabairc" >> "$HOME/.yabairc"
 
-cp "$HOME/dotfiles/dotfiles/.npmrc" "$HOME/.npmrc"
+# echo "setting up git"
+# cp "$HOME/dotfiles/git/.gitignore" "$HOME/.gitignore"
+# cp "$HOME/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+# cp "$HOME/dotfiles/dotfiles/.npmrc" "$HOME/.npmrc"
