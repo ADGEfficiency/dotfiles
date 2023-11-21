d4dad1bfd6d597908b6bd53321f1640fc730f492
Adam Green <adam.green@adgefficiency.com>
2023-11-21 13:21:27 +1300

    feat: indent line

--- nvim/lua/adam/plugins.lua
+++ nvim/lua/adam/plugins.lua
@@ -153,6 +153,20 @@ return packer.startup(function(use)
 	use("farmergreg/vim-lastplace")
 	use("axelf4/vim-strip-trailing-whitespace")
 	use({ "mbbill/undotree" })
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			-- Define a custom highlight group for indent lines
+			vim.api.nvim_set_hl(0, "CustomIndent", { fg = "#6272A4" })
+			-- Setup indent-blankline with the custom highlight group
+			require("ibl").setup({
+				indent = {
+					char = "┆",
+					highlight = { "CustomIndent" }, -- Using the custom highlight group
+				},
+			})
+		end,
+	})
 
 	-- text editing - python
 	use("Vimjas/vim-python-pep8-indent")
