44d0041f59c821dbe9771616034d3af6a00f08b6
Adam Green <adam.green@adgefficiency.com>
2024-12-23 01:59:42 +1300

    feat

--- python/setup-uv.sh
+++ python/setup-uv.sh
@@ -1,1 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
+uv tool install mypy --python 3.11.9
+uv tool install pytest --python 3.11.9
+uv tool install ruff --python 3.11.9
+uv tool install yamllint --python 3.11.9
