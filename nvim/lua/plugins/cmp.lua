614937ff2a726dc663db9d4c61324779a8e6596e
Adam Green <adam.green@adgefficiency.com>
2025-03-26 02:15:40 +1300

    feat

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -48,6 +48,39 @@ return {
 			},
 
 			completion = {
+				documentation = {
+					-- Controls whether the documentation window will automatically show when selecting a completion item
+					auto_show = false,
+					-- Delay before showing the documentation window
+					auto_show_delay_ms = 0,
+					-- Delay before updating the documentation window when selecting a new item,
+					-- while an existing item is still visible
+					update_delay_ms = 50,
+					-- Whether to use treesitter highlighting, disable if you run into performance issues
+					treesitter_highlighting = true,
+					-- Draws the item in the documentation window, by default using an internal treesitter based implementation
+					draw = function(opts)
+						opts.default_implementation()
+					end,
+					window = {
+						min_width = 10,
+						max_width = 80,
+						max_height = 20,
+						border = nil, -- Defaults to `vim.o.winborder` on nvim 0.11+ or 'padded' when not defined/<=0.10
+						winblend = 0,
+						winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+						-- Note that the gutter will be disabled when border ~= 'none'
+						scrollbar = true,
+						-- Which directions to show the documentation window,
+						-- for each of the possible menu window directions,
+						-- falling back to the next direction when there's not enough space
+						direction_priority = {
+							menu_north = { "e", "w", "n", "s" },
+							menu_south = { "e", "w", "s", "n" },
+						},
+					},
+				},
+
 				list = {
 					selection = { auto_insert = true, preselect = false },
 				},
@@ -170,11 +203,6 @@ return {
 						module = "blink.cmp.sources.snippets",
 						name = "snippets",
 						max_items = 3,
-						opts = {
-							search_paths = {
-								"~/dotfiles/nvim/snippets/",
-							},
-						},
 					},
 					ripgrep = {
 						name = "Ripgrep",
