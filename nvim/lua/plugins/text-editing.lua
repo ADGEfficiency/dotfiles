59fb1d30301a365eefe8dfc571eda701cc256365
Adam Green <adam.green@adgefficiency.com>
2025-03-30 04:04:05 +1300

    feat

--- nvim/lua/plugins/text-editing.lua
+++ nvim/lua/plugins/text-editing.lua
@@ -123,15 +123,6 @@ return {
     },
 	},
 	{
-		"iamcco/markdown-preview.nvim",
-		enable = false,
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
-	{
 		"PedramNavid/dbtpal",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
