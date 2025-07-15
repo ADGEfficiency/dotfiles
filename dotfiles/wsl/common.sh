eff1d5373cad39e1e609ec818d1e757c89c5bfd3
Adam Green <adam.green@adgefficiency.com>
2025-07-15 16:10:11 +1200

    feat

--- dotfiles/wsl/common.sh
+++ dotfiles/wsl/common.sh
@@ -1,6 +1,6 @@
 alias docs='cd ~/onedrive/docs'
 alias od='cd ~/onedrive'
 export TODO_DIR="/home/adam/onedrive/docs/project"
-keychain --quiet ~/.ssh/github_ed25519
-keychain --quiet ~/.ssh/azure-devops_id_rsa
+keychain --quiet ~/.ssh/ado
+keychain --quiet ~/.ssh/github
 alias expl='explorer.exe .'
