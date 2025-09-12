3c89d81dc82eaa0cba777f0717f3047f468e4678
Adam Green <adam.green@adgefficiency.com>
2025-09-12 23:13:37 +1200

    feat

--- fish/setup.sh
+++
@@ -1,3 +0,0 @@
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install edc/bass
-fisher install jethrokuan/z
