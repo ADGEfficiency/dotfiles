1fa4f3023edadfe7e5b9d3590342e1b85a7a901c
Adam Green <adam.green@adgefficiency.com>
2025-10-19 18:13:22 +1300

    feat

--- python/setup-uv.sh
+++ python/setup-uv.sh
@@ -5,3 +5,4 @@ uv tool install ruff --python 3.11.9
 uv tool install yamllint --python 3.11.9
 uv tool install pydocstyle --python 3.11.9
 uv tool install sqlfluff --python 3.11.9
+uv tool install pynvim --python 3.11.9
