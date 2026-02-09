00cdc4bff459dc79f9498a86743165e087f895da
Adam Green <adam.green@adgefficiency.com>
2026-02-10 00:26:15 +1300

    feat

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,9 +1,49 @@
 return {
 	{
+		"copilotlsp-nvim/copilot-lsp",
+		init = function()
+			vim.g.copilot_nes_debounce = 500
+			vim.lsp.enable("copilot_ls")
+			vim.keymap.set("n", "<tab>", function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				local state = vim.b[bufnr].nes_state
+				if state then
+					-- Try to jump to the start of the suggestion edit.
+					-- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+					local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+						or (
+							require("copilot-lsp.nes").apply_pending_nes()
+							and require("copilot-lsp.nes").walk_cursor_end_edit()
+						)
+					return nil
+				else
+					-- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
+					return "<C-i>"
+				end
+			end, { desc = "Accept Copilot NES suggestion", expr = true })
+		end,
+	},
+	{
 		"zbirenbaum/copilot.lua",
+		requires = {
+			"copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+		},
 		cmd = "Copilot",
 		build = ":Copilot auth",
 		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				nes = {
+					enabled = true,
+					keymap = {
+						accept_and_goto = "<leader>p",
+						accept = false,
+						dismiss = "<Esc>",
+					},
+				},
+			})
+			require("copilot.suggestion").toggle_auto_trigger()
+		end,
 		opts = {
 			suggestion = { enabled = false },
 			panel = { enabled = false },
@@ -21,14 +61,6 @@ return {
 				yaml = true,
 			},
 		},
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		event = { "BufEnter" },
-		dependencies = { "zbirenbaum/copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup()
-		end,
 	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
