31cc99283e74e1541cb92211953ea1f56d9f2aec
Adam Green <adam.green@adgefficiency.com>
2026-07-04 20:05:57 +1200

    feat

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,5 @@
 vim.opt_local.foldenable = true
 vim.opt_local.foldlevel = 6
+
+-- Shift+Enter: new bullet, indented one level deeper (requires kitty keyboard protocol)
+vim.keymap.set("i", "<S-CR>", "<Plug>(bullets-newline)<Plug>(bullets-demote)", { buffer = true, remap = true })
