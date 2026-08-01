1a2967a52857715efd445b6361d364734615e1fb
Adam Green <adam.green@adgefficiency.com>
2026-08-01 18:40:22 +1200

    feat

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -31,71 +31,54 @@ return {
 			},
 		},
 	},
-	{
-		"olimorris/codecompanion.nvim",
-		version = "^19.0.0",
-		opts = {
-			rules = {
-				default = {
-					description = "AGENTS.md files",
-					files = {
-						"AGENTS.md",
-						"~/dotfiles/config/pi/AGENTS.md",
-					},
-				},
-				opts = {
-					chat = {
-						enabled = true,
-						autoload = "default",
-					},
-				},
-			},
-			interactions = {
-				chat = {
-					adapter = {
-						name = "copilot",
-						model = "claude-sonnet-4.6",
-					},
-					tools = {
-						opts = {
-							default_tools = {
-								"files",
-								"run_command",
-								"get_diagnostics",
-							},
-						},
-					},
-					keymaps = {
-						send = {
-							callback = function(chat)
-								vim.cmd("stopinsert")
-								chat:add_buf_message({ role = "llm", content = "" })
-								chat:submit()
-							end,
-							index = 1,
-							description = "Send",
-						},
-					},
-					inline = {
-						adapter = "copilot",
-						model = "claude-sonnet-4.6",
-					},
-					cmd = {
-						adapter = "copilot",
-						model = "claude-sonnet-4.6",
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			require("config.codecompanion_spinner"):init()
-			require("codecompanion").setup(opts)
-		end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-	},
+
+	-- 		},
+	-- 		interactions = {
+	-- 			chat = {
+	-- 				adapter = {
+	-- 					name = "copilot",
+	-- 					model = "claude-sonnet-4.6",
+	-- 				},
+	-- 				tools = {
+	-- 					opts = {
+	-- 						default_tools = {
+	-- 							"files",
+	-- 							"run_command",
+	-- 							"get_diagnostics",
+	-- 						},
+	-- 					},
+	-- 				},
+	-- 				keymaps = {
+	-- 					send = {
+	-- 						callback = function(chat)
+	-- 							vim.cmd("stopinsert")
+	-- 							chat:add_buf_message({ role = "llm", content = "" })
+	-- 							chat:submit()
+	-- 						end,
+	-- 						index = 1,
+	-- 						description = "Send",
+	-- 					},
+	-- 				},
+	-- 				inline = {
+	-- 					adapter = "copilot",
+	-- 					model = "claude-sonnet-4.6",
+	-- 				},
+	-- 				cmd = {
+	-- 					adapter = "copilot",
+	-- 					model = "claude-sonnet-4.6",
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("config.codecompanion_spinner"):init()
+	-- 		require("codecompanion").setup(opts)
+	-- 	end,
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 	},
+	-- },
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		ft = { "codecompanion" },
